150b13b45ebc389e38bd75f40f7d58a451f22a85
jteh89 <j-teh@hotmail.com>
2014-11-13 19:11:34 +1100

    Began work on Tubby Script

---
+++ Game/Assets/Scripts/Tubby.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tubby : MonoBehaviour {
+
+    enum PLAYER
+    {
+        PLAYER_1,
+        PLAYER_2,
+        PLAYER_COUNT
+    };
+
+    public PLAYER ePlayer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
